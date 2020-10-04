8341a419108e432c4b41e7efab2a6e003d2a02f9
Maxence Gilliot <maxencegilliot@gmail.com>
2020-10-04 23:05:02 +0200

    Updates

---
+++ precache-manifest.55d3adf5141f0b2e9da038c73c1b981a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67facc798864dec58898dc46b76881b3",
+    "url": "/closeoff/index.html"
+  },
+  {
+    "revision": "9a8a87749c652e8c2329",
+    "url": "/closeoff/static/css/main.93ec17ff.chunk.css"
+  },
+  {
+    "revision": "25bf65ea9435cc4a7575",
+    "url": "/closeoff/static/js/2.7276da4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/closeoff/static/js/2.7276da4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8a87749c652e8c2329",
+    "url": "/closeoff/static/js/main.18dfd29b.chunk.js"
+  },
+  {
+    "revision": "26805e17855cdc6d40bd",
+    "url": "/closeoff/static/js/runtime-main.e4bc4cb4.js"
+  }
+]);
